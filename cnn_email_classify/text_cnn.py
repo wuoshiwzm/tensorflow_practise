02fb212b6d6365b387f68536f04bfc72aae1dfec
LAPTOP-SOIG1JSD\wuosh <wuoshiwzm@hotmail.com>
2019-02-21 21:14:32 +0800

    2-21

--- cnn_email_classify/text_cnn.py
+++ cnn_email_classify/text_cnn.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 # 分类网络类
 class TextCNN(object):
     def __init__(self,sequence_length,num_classes,vocab_size,embedding_size,filter_sizes,num_filters,l2_reg_lambda):
-        self_input_x = tf.placeholder(tf.int32,[None,sequence_length],name='input_x')  #N*128
+        self.input_x = tf.placeholder(tf.int32,[None,sequence_length],name='input_x')  #N*128
         self.input_y = tf.placeholder(tf.float32,[None,num_classes],name='input_y')
         self.dropout_keep_prob = tf.placeholder(tf.float32,name='dropout_keep_prob')
 
@@ -12,5 +12,6 @@ class TextCNN(object):
 
         # embedding layer 生成device实例
         with tf.device('cpu:0'),tf.name_scope('embedding'):
-            self.W = tf.Variable
+            self.W = tf.Variable(tf.random_uniform([vocab_size,embedding_size],-1.0,1.0),name="W")
+            self.embedded_chars = tf.nn.embedding_lookup(self.W,self.input_x)
 
